bcde93f3e7acd0b902f03ef8cb28f7e4878a7e38
USL-SZ-392 <priyanshi@unthinkable.co>
2022-12-14 12:53:01 +0530

    ui changes

--- pages/index.js
+++ pages/index.js
@@ -2,55 +2,48 @@ import React from "react";
 import Head from "next/head";
 import styles from "../styles/Todo.module.css";
 import { NextSeo } from "next-seo";
+import {Html} from "next/document";
 
 
 function todoList({ items }) {
   return (
-    <div
-      aria-labelledby="todos-wrapper"
-      className={styles.wrapper}
-    >
-      <NextSeo
-        title="todo list"
-        description="Todo list for all the task you have to do in a year"
-      />
-      <Head>
-        <title aria-labelledby="todos-title" >
-          Todo List
-        </title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <h1 aria-labelledby="todos-heading" >
-        Todo List
-      </h1>
-      <div
-        role="tree"
-        aria-labelledby="todos-content"
-       
-      >
-        {items.map((item) => {
-          return (
-            <div
-              key={item.id}
-              role="treeitem"
-              aria-labelledby="todos-input"
-              className={styles.listitem}
-            >
-              <input
-                type="checkbox"
-                role="checkbox"
-                readOnly
-                id={item.id}
-                name={item.title}
-                checked={item.completed}
-                aria-checked={`${item.completed}`}
-              ></input>
-              <span className={styles.text}>{item.title}</span>
-            </div>
-          );
-        })}
+      <div aria-labelledby="todos-wrapper" className={styles.wrapper}>
+        <NextSeo
+          title="todo list"
+          description="Todo list for all the task you have to do in a year"
+        />
+        <Head>
+          <title aria-labelledby="todos-title">Todo List</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <h1 aria-labelledby="todos-heading">Welcome To Your Todo List</h1>
+        <div
+          role="tree"
+          aria-labelledby="todos-content"
+          className={styles.list}
+        >
+          {items.map((item) => {
+            return (
+              <div
+                key={item.id}
+                role="treeitem"
+                aria-labelledby="todos-input"
+                className={styles.listitem}
+              >
+               
+                {item.completed === false ? (
+                  <label className={styles.textfalse}>{item.title}</label>
+                ) : (
+                  <label className={styles.texttrue}>{item.title}</label>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
   );
 }
 
